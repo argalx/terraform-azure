22467049a87b64260f2d12aee9e0183c4ec5524f
Argal <arvin.bonggal@gmail.com>
2024-05-16 15:25:42 +0800

    Add .gitignore

--- main.tf
+++ main.tf
@@ -9,9 +9,3 @@ module "resource_group1" {
   resource_group_name = "agxtest-rg1"
   resource_group_location = "Australia East"
 }
-
-module "resource_group2" {
-  source = "./modules/base/resources/resource-group"
-  resource_group_name = "agxtest-rg2"
-  resource_group_location =  "West US"
-}
